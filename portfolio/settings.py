31eb721c683345bf23a0467a6c2b0363d250b320
kibirio <ruiruharrison047@gmail.com>
2021-03-25 00:49:34 +0300

    settings debug status

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -29,7 +29,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = 'sg7n$0jk#4a951u+h#8e$zhc_xzo&qe&e^2^hpnu&^ti_hwgsv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'haryx.herokuapp.com']
 
