90f45948a726075ca982f0d63a072bb15fbc8162
kibirio <ruiruharrison047@gmail.com>
2021-03-25 16:38:47 +0300

    changes on the about positioning

--- main/context_processors.py
+++ main/context_processors.py
@@ -3,5 +3,5 @@ import datetime
 def get_current_year_to_context(request):
     current_datetime = datetime.datetime.now()
     return {
-        'current_year': current_datetime.now
+        'current_year': current_datetime.date
     }
